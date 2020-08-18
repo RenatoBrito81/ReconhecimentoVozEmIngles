b20a11f63c6dc91dc3f00879f0e7d7e940b73b75
Renato Brito <rb.renatobrito@gmail.com>
2020-08-17 21:29:17 -0300

    Refatorado o codigo.

--- js/funcoes-api.js
+++ js/funcoes-api.js
@@ -1,97 +1,98 @@
+//Dados para executar o jogo
 var engine = {
     "cores": ["green","purple","pink","red","yellow","orange","gray", "black","white"],
-    "hexadecimais": {
-        "green": "#02EF00",
-        "purple": "#790093",
-        "pink": "#F02A7E",
-        "red": "#E90808",
-        "yellow": "#E7D703",
-        "orange": "#F16529",
-        "gray": "#EBEBEB",
-        "black": "#141414",
-        "white": "#ffffff"        
-    },
+    "hexadecimais": ["#02EF00", "#790093", "#F02A7E", "#E90808", "#E7D703", "#F16529", "#EBEBEB", "#141414", "#FFFFFF"],
     "moedas": 0
 }
 
+//Referências aos áudios do jogo
 const audioMoeda = new Audio("../audio/moeda.mp3");
 const audioErrou = new Audio("../audio/errou.mp3");
 
+//Função para sortear uma cor
 function sortearCor(){
+    //Define o índice da cor sorteada
     var indiceCorSorteada = Math.floor(Math.random() * engine.cores.length);
-    var legendaCorDaCaixa = document.getElementById("cor-na-caixa");
-    var nomeCorSorteada = engine.cores[indiceCorSorteada];
-    legendaCorDaCaixa.innerText = nomeCorSorteada.toUpperCase();
-    var corSorteada = engine.cores[indiceCorSorteada];
 
-    return engine.hexadecimais[nomeCorSorteada];
+    //Atribui a cor sorteada
+    document.getElementById("cor-na-caixa").innerText = engine.cores[indiceCorSorteada].toUpperCase();
+    document.getElementById("cor-atual").style.backgroundColor = engine.hexadecimais[indiceCorSorteada];
+    document.getElementById("cor-atual").style.backgroundImage = "url('/img/caixa-fechada.png')";
+    document.getElementById("cor-atual").style.backgroundSize = "100%";
 }
+sortearCor();
 
-function aplicarCorNaCaixa(nomeDaCor){
-    var caixaDasCores = document.getElementById("cor-atual");
-    caixaDasCores.style.backgroundColor = nomeDaCor;
-    caixaDasCores.style.backgroundImage = "url('/img/caixa-fechada.png')";
-    caixaDasCores.style.backgroundSize = "100%";
-}
-aplicarCorNaCaixa(sortearCor());
-
+//Função para atualizar a pontução
 function atualizarPontuacaoAtual(valor){
-    var pontuacao = document.getElementById("pontuacao-atual");
+    //Soma o valor no contador de moedas
     engine.moedas += valor;
     
+    //Verifica se errou a cor
     if(valor < 0 ){
+        //Toca o áudio de erro
         audioErrou.play();
     }
     else{
+        //Toca o áudio de acerto
         audioMoeda.play();
     }
 
-    pontuacao.innerText = engine.moedas
+    //Seta a pontução
+    document.getElementById("pontuacao-atual").innerText = engine.moedas
 }
 
-
+//Referência a API de reconhcimento de voz
 var botaoGravador = document.getElementById("btn-responder");
-var transcricaoAudio = "";
-var respostaCorreta = "";
 var gravador;
 
+//Verifica a versão da API de reconhecimento de voz
 if(window.SpeechRecognition || window.webkitSpeechRecognition){
     var speechAPI =  window.SpeechRecognition || window.webkitSpeechRecognition;
     gravador = new speechAPI();
 
+    //Configura a API
     gravador.continuous = false;
     gravador.lang = "en-US";
 
+    //Configura o evento start
     gravador.onstart = function(){
         botaoGravador.innerText = "Estou ouvindo...";
         botaoGravador.style.backgroundColor = "white";
         botaoGravador.style.color = "black";
     }
 
+    //Configura o evento end
     gravador.onend = function(){
         botaoGravador.innerText = "RESPONDER";
         botaoGravador.style.backgroundColor = "transparent";
         botaoGravador.style.color = "white";
     }
 
+    //Configura o evento result
     gravador.onresult = function(evento){
-        transcricaoAudio = evento.results[0][0].transcript.toUpperCase();
-        respostaCorreta = document.getElementById("cor-na-caixa").innerText.toUpperCase();
+        var transcricaoAudio = evento.results[0][0].transcript.toUpperCase();
+        var respostaCorreta = document.getElementById("cor-na-caixa").innerText.toUpperCase();
 
+        //Verifica se a cor foi acertada
         if(transcricaoAudio === respostaCorreta){
-            atualizarPontuacaoAtual(1);
+            //Atualiza a pontuação
+        atualizarPontuacaoAtual(1)
         }
         else{
-            atualizarPontuacaoAtual(-1);
+            //Atualiza a pontuação
+        atualizarPontuacaoAtual(-1)
         }
 
-        aplicarCorNaCaixa(sortearCor());
+        //Sorte uma nova cor
+        sortearCor();
     }
 }
 else{
     alert("Navegador não possui suporte ao reconhecimento de voz.\nPor favor abra a página em outro nagevador.");
 }
 
+//Adiciona o evento ao botão responder
 botaoGravador.addEventListener("click", function(e){
+    //Inicia a gravação do áudio
     gravador.start();
 });
